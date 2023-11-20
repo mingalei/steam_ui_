65cbf77a3f16352297aa6df6abf0664c104db9b5
mingalei <mingalei33@gmail.com>
2023-11-20 23:48:30 +0300

    update

--- src/test/java/config/WebConfig.java
+++ src/test/java/config/WebConfig.java
@@ -2,7 +2,9 @@ package config;
 
 import org.aeonbits.owner.Config;
 
-
+@Config.Sources({
+        "classpath:${env}.properties",
+})
     public interface WebConfig extends Config {
 
         @Key("browser")
